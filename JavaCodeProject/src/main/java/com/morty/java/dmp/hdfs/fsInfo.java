1c4f56b06aabe8f568a9edd4a2f3b51b6fc032aa
liang du <xiaokugua250@hotmail.com>
2016-05-14 17:00:59 +0800

    提交hdfs文件基本操作

---
+++ JavaCodeProject/src/main/java/com/morty/java/dmp/hdfs/fsInfo.java
@@ -0,0 +1,12 @@
+package com.morty.java.dmp.hdfs;
+
+/**
+ * Created by duliang on 2016/5/13.
+ */
+public class fsInfo {
+
+    public static String DATA_SOURCE_URI="hdsf://dev:9000/hdfs/data";
+    public static String DATA_DESTINATION_URI="hdfs://dev:9000/hdfs/tmp";
+
+
+}
